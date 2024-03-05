105c98dca70b10cc92651224daec3f88eae7fb37
ARIBILGI <ARIBILGI@ARIBILGI-Y1>
2024-03-05 12:17:03 +0300

    some changes

--- P06_Arrays/DiziMetotlari.cs
+++ P06_Arrays/DiziMetotlari.cs
@@ -28,7 +28,7 @@ namespace P06_Arrays
 
             #endregion
 
-            #region diğer bir kullanım
+            #region 2. diğer bir kullanım
             //int[][] dizi16 = new int[3][];
 
             //dizi16[0] = new int[] { 1, 2, 3 }; 
@@ -43,26 +43,84 @@ namespace P06_Arrays
 
             #endregion
 
-            #region 2. Create Instanse
+            #region 3. Create Instanse
 
-            // int türünden 5 elemanlı "dizi17" isimli bir dizi yaratınız.
+            //// int türünden 5 elemanlı "dizi17" isimli bir dizi yaratınız.
 
-            Array dizi17 = Arrays.CreatInstance(typeof(int), 5);
+            //Array dizi17 = Arrays.CreatInstance(typeof(int), 5);
             
-            // int türünden 3x2x5 boyutunda 3 boyutlu dizi yaratımı
-            Array dizi18= Arrays.CreatInstance(typeof(int), 3,2,5);
+            //// int türünden 3x2x5 boyutunda 3 boyutlu dizi yaratımı
+            //Array dizi18= Arrays.CreatInstance(typeof(int), 3,2,5);
 
 
-            //ilginç kullanım... yaratılırken ilk değerlerin atanması
-            int[] dizi19 = new int[5] { 2,3,4,5,6};
+            ////ilginç kullanım... yaratılırken ilk değerlerin atanması
+            //int[] dizi19 = new int[5] { 2,3,4,5,6};
 
-            // Gerçekten ilginç
+            //// Gerçekten ilginç
 
-            // dizi20 int ve 2x3x4x5x6 olarak 5 boyutlu hal aldı
-            Array dizi20 = P06_Arrays.CreateInstance(typeof(int), dizi19); //dinamik bir dizi yaratmış oldum. Yukarıdaki sayıları direkt dizi ismiyle ldım!!!
+            //// dizi20 int ve 2x3x4x5x6 olarak 5 boyutlu hal aldı
+            //Array dizi20 = P06_Arrays.CreateInstance(typeof(int), dizi19); //dinamik bir dizi yaratmış oldum. Yukarıdaki sayıları direkt dizi ismiyle ldım!!!
 
 
             #endregion
+
+            #region 4. Dizi Kopyalama
+            //int[] dizi22 = { 1, 2, 3, 4 };
+            //int[] dizi23 = new int[10];
+
+            //dizi22.CopyTo(dizi23, 3); //dizi22'nin tüm elemanları dizi23'e 3. indexten itibaren aktarılıyor/kopyalanıyor.
+
+            ////*
+
+            //int[] dizi24 = { 1, 2, 3, 4 };
+            //int[] dizi25= new int[10];
+
+            //Arrays.CopyTo(dizi25, 3); // Kopyalama işlemi normalde 0. indexten başlar. Burada 3 tane eleman dizi24 -> dizi25 e kopyalanıyor. 
+
+            #endregion
+
+            #region 5. Dizi Sıralama
+
+            Array sehirler = Array.CreateInstance(typeof(string), 8);
+
+            sehirler.SetValue("İstanbul", 0);
+            sehirler.SetValue("Ankara", 1);
+            sehirler.SetValue("İzmir", 2);
+            sehirler.SetValue("Bursa", 3);
+            sehirler.SetValue("Adıyaman", 4);
+            sehirler.SetValue("Muğla", 5);
+            sehirler.SetValue("Trabzon", 6);
+            sehirler.SetValue("Rize", 7);
+
+            Console.WriteLine("Sırasız halde dizi\n\n");
+
+            foreach (string sehir in sehirler)
+                Console.Write(sehir + "  ");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Alfabetik sıralı halde dizi (A-Z)\n\n");
+
+            Arrays.Sort(sehirler);
+
+            foreach (string sehir in sehirler)
+                Console.Write(sehir + "  ");
+
+            Console.WriteLine("");
+            Console.WriteLine("Alfabetik sıralı halde dizi (Z-A)\n\n");
+
+            Arrays.Reverse(sehirler);
+            
+            foreach (string sehir in sehirler)
+                Console.Write(sehir + "  ");
+
+
+
+
+
+
+            #endregion
+
 
 
             Console.ReadKey();

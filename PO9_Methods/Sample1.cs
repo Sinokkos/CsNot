0d8d1e0bf4c335e8e52920c8d2d54766f442e0a7
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-06 12:00:32 +0300

    some changes

---
+++ PO9_Methods/Sample1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO9_Methods
+{
+    internal class Sample1
+    {
+       public static void Main(string[] args)
+       {
+    
+        
+            
+        Console.ReadKey();
+
+
+       }
+    }
+}

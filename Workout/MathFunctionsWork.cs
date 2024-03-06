0d30b157c7177ef6c6378a16df780806bf95be37
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-06 10:00:45 +0300

    some changes

--- Workout/MathFunctionsWork.cs
+++ Workout/MathFunctionsWork.cs
@@ -10,12 +10,49 @@ namespace Workout
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine($"Yuvarlama 2 - {Math.Round(0.5)}\n\n");   // . dan sonra gelen sayı 5 <= ise alt sayıya yuvarlanır.
-            Console.WriteLine($"Yuvarlama 2 - {Math.Round(3.01)}\n\n");
-            Console.WriteLine($"Yuvarlama 3 - {Math.Round(3.49)}\n\n");
-            Console.WriteLine($"Yuvarlama 4 - {Math.Round(3.51)}\n\n");
-            Console.WriteLine($"Yuvarlama 5 - {Math.Round(3.50)}\n\n"); // . dan sonra gelen sayı 5 <= ise alt sayıya artarak yuvarlanır 
-                                                                        // Bu örnekte 0 kendini bir arttırarak 5'e eklenirse 6 değeri bizi üst sayıya yuvarlar.
+
+            #region 1. Sayı Yuvarlama (Round)
+
+
+            //Console.WriteLine($"Yuvarlama 2 - {Math.Round(0.5)}\n\n");   // . dan sonra gelen sayı 5 <= ise alt sayıya yuvarlanır.
+            //Console.WriteLine($"Yuvarlama 2 - {Math.Round(3.01)}\n\n");
+            //Console.WriteLine($"Yuvarlama 3 - {Math.Round(3.49)}\n\n");
+            //Console.WriteLine($"Yuvarlama 4 - {Math.Round(3.51)}\n\n");
+            //Console.WriteLine($"Yuvarlama 5 - {Math.Round(3.50)}\n\n"); // . dan sonra gelen sayı 5 <= ise alt sayıya artarak yuvarlanır 
+            //                                                            // Bu örnekte 0 kendini bir arttırarak 5'e eklenirse 6 değeri bizi üst sayıya yuvarlar.
+            #endregion
+
+            #region 2. Vücut Kitle Endex hesaplama örneği
+
+            //double kilo = 62.3;
+            //double boy = 1.58;
+            //double vke;
+
+            //vke = kilo / (boy * boy);
+
+            //Console.WriteLine("Vücut Kilo Endexi Hesaplama (VKE)");
+            //Console.WriteLine($"Kilo: {kilo}kg");
+            //Console.WriteLine($"Boy : {boy}m");
+            //Console.WriteLine($"VKE: {vke: 0.#}");
+
+
+
+            #endregion
+
+            #region 3. String Örnekleri
+
+            string isim = "Sinem";
+            string selamlama = $"Selam {isim}!";
+
+            Console.WriteLine(selamlama);
+
+
+
+            #endregion
+
+
+
+            Console.ReadKey();
         }
     }
 }

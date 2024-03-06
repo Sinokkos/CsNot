7e26081ece5551661b6d75393b8b99bb2ee4f419
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-06 11:10:00 +0300

    some changes

---
+++ PO8_ExceptionHandling/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region 1. Hata oluşturabilecek bir durum
+
+        //int sayi1 = 100;
+        //int sayi2 = 0;
+
+        //int sonuc= sayi1/sayi2;
+
+        //Console.WriteLine($"Bölme işleminin sonucu: {sonuc}");
+
+
+
+
+
+        #endregion
+
+        #region 2. Hata Avcısı
+        //int sayi1 = 100;
+        //int sayi2 = 0;
+
+        //avcı
+
+        //try
+        //{
+        //    int sonuc = sayi1 / sayi2;
+        //    Console.WriteLine($"Bölme işlemin sonucu: {sonuc}");
+        //}
+        //catch (Exception)
+        //{
+
+        //    //Console.WriteLine("Valla arkadaş... bir yerlerde terslik var");
+
+        //    if (sayi1 !=0 && sayi2==0)
+        //    {
+        //        sayi2 = 1;
+        //        int sonuc=sayi2 / sayi1;
+        //        Console.WriteLine($"Durum düzeltildi...{sonuc}");
+        //    }
+        //}
+
+        #endregion
+
+
+
+        Console.ReadKey();
+    }
+}

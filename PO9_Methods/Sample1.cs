573acfa13ac4edb1daa0dd9c53fadade28406672
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-06 12:12:23 +0300

    some changes

--- PO9_Methods/Sample1.cs
+++ PO9_Methods/Sample1.cs
@@ -10,12 +10,48 @@ namespace PO9_Methods
     {
        public static void Main(string[] args)
        {
-    
-        
-            
-        Console.ReadKey();
+
+            // Hack 01
+            // Dikdörtgen alanını hesaplayan bir metodun 4 farklı şeklinin incelenmesi
+
+
+            #region 1. Geriye değer döndürmeyen ve parametre almayan metot
+
+            Hesapla();
+
+
+            #endregion
+
+
+
+
+            Console.ReadKey();
 
 
        }
+
+        #region Methodlar
+
+        // region 01
+        private static void Hesapla() 
+        {
+            Console.WriteLine("Lütfen kısa kenarı giriniz :");
+            int kk=Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Lütfen uzun kenarı giriniz :");
+            int uk = Convert.ToInt32(Console.ReadLine());
+
+            int alan = kk * uk;
+
+            Console.WriteLine($"\nDikdörtgenin alanı = {alan}");
+
+
+        }  
+
+
+
+        #endregion
+
     }
+
 }

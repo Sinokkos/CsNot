a6114b1ab4b9f0661d0397ec904693cfcfbe068e
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-06 14:43:38 +0300

    some changes

---
+++ PO9_Methods/Sample2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO9_Methods
+{
+    internal class Sample2
+    {
+        public static void Main(string[] args)
+        {
+            #region Metotlarda aşırı yükleme (method overloading)
+
+
+            Console.WriteLine($"2li toplama sonucu = {ToplamaIslemi(34,56)}\n\n");
+            Console.WriteLine($"3lü toplama sonucu = {ToplamaIslemi(34,56,12)}\n\n");
+            Console.WriteLine($"4lü toplama sonucu = {ToplamaIslemi(34,56,12,33)}\n\n");
+            
+            
+
+            #endregion
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+
+        #region Methods
+
+        private static int ToplamaIslemi(int psayi1, int psayi2)
+        {   // ikili toplama için 
+           return psayi1 + psayi2;
+        }
+
+        private static int ToplamaIslemi(int psayi1, int psayi2, int psayi3) 
+        {  // üçlü toplama için
+          return psayi1 + psayi2 + psayi3;
+        
+        }
+
+        private static int ToplamaIslemi(int psayi1, int psayi2, int psayi3, int psayi4)
+        {  // dörtlü toplama için
+            return psayi1 + psayi2 + psayi3 + psayi4;
+
+        }
+        #endregion
+
+
+    }
+}
